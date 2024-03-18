6efdec89e8cd9e63fffe850a62068f8d9f8c6a50
Aaron Mineen <amineen64@hotmail.com>
2024-03-18 18:27:35 +0200

    Add string conversion functions

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,36 @@ fn main() {
 
     let datetime_array = iso_time_string.split('T').collect::<Vec<&str>>();
 
-    println!("Time: {}", datetime_array[1]);
+    let s = "Aaron Mineen";
+    let binaries = string_to_binaries(s);
+    println!("{:?}", binaries);
 
-    println!("Current time is: {}", current_time);
-    println!("{}", iso_time_string);
+    let binary_vector = string_to_binary_vector(s);
+    println!("{:?}", binary_vector);
+
+    let string = binary_vector_to_string(binary_vector);
+    println!("{:?}", string);
+}
+
+fn string_to_binaries(s: &str) -> String {
+    s.chars()
+        .map(|c| format!("{:08b}", c as u8))
+        .collect::<Vec<String>>()
+        .join("")
+}
+
+fn string_to_binary_vector(s: &str) -> Vec<String> {
+    s.chars()
+        .map(|c| format!("{:08b}", c as u8))
+        .collect::<Vec<String>>()
+}
+
+fn binary_vector_to_string(binary_vector: Vec<String>) -> String {
+    binary_vector
+        .iter()
+        .map(|binary| {
+            let decimal = i32::from_str_radix(binary, 2).unwrap();
+            char::from_u32(decimal as u32).unwrap()
+        })
+        .collect()
 }
